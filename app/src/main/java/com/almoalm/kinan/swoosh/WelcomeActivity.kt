d4a067162898a70a2bf3e81af62bc458d3b5093a
Matrix <matrixofkings@gmail.com>
2019-02-06 22:32:35 +0200

    Init Commit

---
+++ app/src/main/java/com/almoalm/kinan/swoosh/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.almoalm.kinan.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
